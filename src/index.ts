7a2f274e8f46d7f6d1944b0e63177cda57228481
Shai Reznik <shairez@users.noreply.github.com>
2020-09-29 15:44:49 +0300

    chore: removed `ObserverSpyWithSubscription` and `subscribeAndSpyOn`
    please use `SubscriberSpy` and `subscribeSpyTo` instead
    
    BREAKING CHANGE: removed `ObserverSpyWithSubscription` and `subscribeAndSpyOn`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { ObserverSpy } from './observer-spy';
-export { ObserverSpyWithSubscription, SubscriberSpy } from './subscriber-spy';
-export { subscribeSpyTo, subscribeAndSpyOn } from './subscribe-spy-to';
+export { SubscriberSpy } from './subscriber-spy';
+export { subscribeSpyTo } from './subscribe-spy-to';
 export { fakeTime } from './fake-time';
 export { autoUnsubscribe, queueForAutoUnsubscribe } from './auto-unsubscribe';
